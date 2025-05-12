16bc9a46cbcbd271ca8090cbd1762eb37cd11a46
hasan-aydinoglu <hasan.aydng@gmail.com>
2025-05-12 12:19:08 +0300

    trio-web

---
+++ app.js
@@ -0,0 +1,10 @@
+function startGame() {
+    alert("Oyun başlıyor!");
+    // Buraya oyun ekranına yönlendirme ekleyebilirsin.
+  }
+  
+  function register() {
+    alert("Üyelik sayfasına yönlendiriliyorsunuz.");
+    // Üyelik sayfasına yönlendirme buraya eklenebilir.
+  }
+  
